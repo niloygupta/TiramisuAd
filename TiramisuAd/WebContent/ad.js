e6fc99d14d163d7d28f6ccd00b49d4d3b932498f
niloygupta <niloygupta@gmail.com>
2015-03-09 01:04:30 -0400

    Fixed cookie logic

--- TiramisuAd/WebContent/ad.js
+++ TiramisuAd/WebContent/ad.js
@@ -5,8 +5,18 @@ function getMoreInfo()
 	window.location.assign(customURL);
 }
 
-function addBookToCart(asin,devId,traceId) 
+function addBookToCart(event) 
 {
+	
+	/*asin = getCookie("asin");
+	devId = getCookie("devId");
+	traceId = getCookie("traceId");*/
+	
+	asin = sessionStorage.getItem('asin');
+	devId = sessionStorage.getItem('devId');
+	traceId = sessionStorage.getItem('traceId');
+	
+
 	$.ajax({
         type: "GET",
         url: "http://ec2-54-165-114-150.compute-1.amazonaws.com:8080/ajServer/add",
@@ -28,8 +38,18 @@ function addBookToCart(asin,devId,traceId)
 	behaviourUpdate(asin,devId,'addcart',traceId);
 }
    
-    function goToCart(asin,devId,traceId) 
+    function goToCart(event) 
     {
+    	
+    	/*asin = getCookie("asin");
+    	devId = getCookie("devId");
+    	traceId = getCookie("traceId");*/
+    	
+    	asin = sessionStorage.getItem('asin');
+   		devId = sessionStorage.getItem('devId');
+   		traceId = sessionStorage.getItem('traceId');
+
+    	
     	$.ajax({
             type: "GET",
         url: "http://ec2-54-165-114-150.compute-1.amazonaws.com:8080/ajServer/add",
@@ -38,7 +58,11 @@ function addBookToCart(asin,devId,traceId)
         data: 'mtype=shopping&devIdentifier='+devId,
         success: function (data) {
         	if(data.result=="success")
-        		window.open(data.purl);	
+        	{
+        		window.location.href = data.purl;
+        		window.location.assign(data.purl);
+        	}
+        		//window.open(data.purl);	
         },
         error: function (err) {
         }
@@ -59,7 +83,7 @@ function behaviourUpdate(asin,devId,event,traceId)
 		});
 } 
 
-function traceUpdate(devId,lat,longitude,trace_id) 
+function traceUpdate(devId,lat,long,traceId) 
 {
 	$.ajax({
         type: "GET",
@@ -75,12 +99,66 @@ function getTraceId(devId)
 	if (!Date.now) {
 	    Date.now = function() { return new Date().getTime(); }
 	}
-	var traceId = devId+''+ Date.now; 
-    	return traceId;
-    }
+	var traceId = devId+ Date.now(); 
+    return traceId;
+}
+function getDeviceId()
+{
+	var devId;
+	if($.cookie('devId') == null)
+	{
+		genDevId(function(devId){$.cookie('devId',devId);});
+		alert($.cookie('devId'));
+	}
+	
+	return $.cookie('devId');
+}
+function genDevId(callback)
+{
+    $.ajax({
+        type: "GET",
+        url: "http://ec2-54-165-114-150.compute-1.amazonaws.com:8080/ajServer/trace",
+        crossDomain: true,
+        dataType: "jsonp",
+        data: 'mtype=getUserId',
+        success: function (data) {
+        	//alert(data.devId);
+        	devId = data.devId;
+        	alert(devId);
+        	callback(devId);
+        }
+});
+
+}
     
     function closeAd()
     {
-    	close();
+    	//();
+    	//self.close ();
+    	//open(location, '_self').close();
+    	//window.history.back();
+    	window.close();
     }
+    
+    
+    function setCookie(cname,cvalue,exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname+"="+cvalue+"; "+expires;
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    
    
